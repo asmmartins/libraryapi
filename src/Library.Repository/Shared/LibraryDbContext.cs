f0c143a29bcfa3347d64a471ece66ec9b4de6545
Alex Martins <asimartins@icatuseguros.com.br>
2020-11-02 19:07:54 -0300

    feat: Incluido EF Core.

---
+++ src/Library.Repository/Shared/LibraryDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Library.Domain.Authors;
+using Library.Domain.BookAuthors;
+using Library.Domain.Books;
+using Library.Domain.BookSubjects;
+using Library.Domain.Subjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Repository.Shared
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Book> Books { get; set; }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(t => new { t.BookId, t.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(pt => pt.Book)
+                .WithMany(p => p.BookAuthors)
+                .HasForeignKey(pt => pt.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(pt => pt.Author)
+                .WithMany(t => t.BookAuthors)
+                .HasForeignKey(pt => pt.AuthorId);
+
+            modelBuilder.Entity<BookSubject>()
+                .HasKey(t => new { t.BookId, t.SubjectId });
+
+            modelBuilder.Entity<BookSubject>()
+                .HasOne(pt => pt.Book)
+                .WithMany(p => p.BookSubjects)
+                .HasForeignKey(pt => pt.BookId);
+
+            modelBuilder.Entity<BookSubject>()
+                .HasOne(pt => pt.Subject)
+                .WithMany(t => t.BookSubjects)
+                .HasForeignKey(pt => pt.SubjectId);
+        }
+    }
+}

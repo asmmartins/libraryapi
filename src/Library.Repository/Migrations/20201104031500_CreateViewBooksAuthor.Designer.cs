0155130c7fbac05e5bac5a7974e3088fb6c18477
Alex Martins <asimartins@icatuseguros.com.br>
2020-11-04 04:38:45 -0300

    feat: Criado a migration da view que obtem o total de livros por author.

---
+++ src/Library.Repository/Migrations/20201104031500_CreateViewBooksAuthor.Designer.cs
@@ -0,0 +1,13 @@
+﻿using Library.Repository.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Repository.Migrations
+{
+    [DbContext(typeof(LibraryDbContext))]
+    [Migration("20201104031500_CreateViewBooksAuthor")]
+    partial class CreateViewBooksAuthor
+    {
+    }
+}
